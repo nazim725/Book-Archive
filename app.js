583e6e6e51c4bde46f478a27a1d9571a09290cb7
Nazim-Habib <c161067@ugrad.iiuc.ac.bd>
2021-09-02 20:22:07 +0600

    updated

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+// Function for search books
+
 const searchBooks=()=>{
     const inputField=document.getElementById('input-field');
     const searchText=inputField.value;
-    const url=`http://openlibrary.org/search.json?q=${searchText}`;
+    const url=`https://openlibrary.org/search.json?q=${searchText}`;
     fetch(url)
     .then(res=>res.json())
     .then(data=>{
@@ -9,24 +11,23 @@ const searchBooks=()=>{
         const numberFound=displayNumber(data.numFound);
         
     })
-    .catch(res=>alert('please try again later'))
+
     inputField.value=''
 
 
 }
+// Function for display Details of Books
+
 const displayBooksdetails=(books)=>{
     const searchResult=document.getElementById('search-result');
     searchResult.textContent='';
     books.forEach(element => {
-        const fetch=`https://covers.openlibrary.org/b/id/${element.cover_i}-M.jpg`;
+        const path=`https://covers.openlibrary.org/b/id/${element.cover_i}-M.jpg`;
         const div=document.createElement('div');
         div.classList.add('col');
         div.innerHTML=`
         <div class="card h-100 p-4 shadow rounded rounded-3 mx-4 bg-primary text-white border border-3">
-        
-        <img  src="${fetch}" height="300"></img>
-      
-        
+        <img  src="${path}" height="300"></img>
         <div class="card-body">
         <p class="fw-bold"><span> <h3> Book Name:</h3> </span>${element.title} </p>
         <p class="fw-bold"><span> <h3> Authors Name:</h3> </span>${element.author_name} </p>
@@ -42,16 +43,16 @@ const displayBooksdetails=(books)=>{
     });
 }
 
-
+// Function for display search result
 const displayNumber=(numbers)=>{
     const searchNumber=document.getElementById('search-number');
     searchNumber.textContent=''
     let h5=document.createElement('h5');
-    h5.classList.add('w-50','mx-auto','text-center','text-success','p-3')
+    h5.classList.add( 'w-50','mx-auto','text-center','text-white','p-3')
     h5.classList.add()
-    h5.innerText=`Total Search result Found: ${numbers}`;
+    h5.innerText=`Total Search Result Found: ${numbers}`;
     if(numbers==0){
-        h5.innerText=` sorry, No Matched Result
+        h5.innerText=` Sorry, No Matched Result
         please Input a Right Books Name!!!!!`; 
     }
     searchNumber.appendChild(h5)
