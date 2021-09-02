ed486f38f22b3db79fce8bc8f4a7b6954f57d902
Nazim-Habib <c161067@ugrad.iiuc.ac.bd>
2021-09-02 13:14:03 +0600

    added

--- app.js
+++ app.js
@@ -5,21 +5,11 @@ const searchBooks=()=>{
     fetch(url)
     .then(res=>res.json())
     .then(data=>{
-        const booksdetails=displayBooksdetails(data.docs);
+        const booksdetails=displayBooksdetails(data.docs.slice(0,20));
         const numberFound=displayNumber(data.numFound);
         
-        
-       
-
     })
     .catch(res=>alert('please try again later'))
-    // .catch(res=>{
-    //     const result=document.getElementById('error-message');
-    //     const p=document.createElement("p");
-    //     p.innerText="Please Type Right Name"
-    //     result.appendChild(p)
-    // });
-    
     inputField.value=''
 
 
@@ -27,21 +17,22 @@ const searchBooks=()=>{
 const displayBooksdetails=(books)=>{
     const searchResult=document.getElementById('search-result');
     searchResult.textContent='';
-    // console.log(books.docs)
     books.forEach(element => {
-        // console.log(element.cover_i)
-        fetch(`https://covers.openlibrary.org/b/id/${element.cover_i}-L.jpg`)
-        .then(res=>res.json())
-        .then(data=>console.log(data.cover_i))
-
+        const fetch=`https://covers.openlibrary.org/b/id/${element.cover_i}-M.jpg`;
         const div=document.createElement('div');
         div.classList.add('col');
         div.innerHTML=`
-        <div class="card h-100 p-4 shadow rounded rounded-3 mx-4 bg-primary text-white">
+        <div class="card h-100 p-4 shadow rounded rounded-3 mx-4 bg-primary text-white border border-3">
+        
+        <img  src="${fetch}" height="300"></img>
+      
+        
+        <div class="card-body">
         <p class="fw-bold"><span> <h3> Book Name:</h3> </span>${element.title} </p>
         <p class="fw-bold"><span> <h3> Authors Name:</h3> </span>${element.author_name} </p>
         <p class="fw-bold"><span> <h3> First Published Year:</h3> </span> ${element.first_publish_year}</p>
         <p class="fw-bold"><span> <h3> Publisher:</h3> </span> ${element.publisher} </p>
+        </div>
         </div>
         `;
 
@@ -51,20 +42,13 @@ const displayBooksdetails=(books)=>{
     });
 }
 
-// const displayImg=(img)=>{
-//     const url=`https://covers.openlibrary.org/b/id/${img}-M.jpg`;
-//     fetch(url)
-//     .then(res=>res.json())
-//     .then(data=>console.log(data))
-    
 
-// }
 const displayNumber=(numbers)=>{
     const searchNumber=document.getElementById('search-number');
     searchNumber.textContent=''
     let h5=document.createElement('h5');
-    h5.classList.add('text-center')
-    h5.classList.add('text-success')
+    h5.classList.add('w-50','mx-auto','text-center','text-success','p-3')
+    h5.classList.add()
     h5.innerText=`Total Search result Found: ${numbers}`;
     if(numbers==0){
         h5.innerText=` sorry, No Matched Result
